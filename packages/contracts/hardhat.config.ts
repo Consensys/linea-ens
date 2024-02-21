93fd91ac0e557b6324d0cf3fbbe91aa4244131f7
Julink <julien.fontanel12@gmail.com>
2024-02-21 17:28:34 +0100

    feat: upgrade Mimc lib to latest version + add hardhat storage layout plugin

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -4,6 +4,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-storage-layout";
 
 const hardhatPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
