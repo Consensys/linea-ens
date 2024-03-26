a57badd15bf667c5f31690f3a2d679223848b542
wantedsystem <amine.harty@consensys.net>
2024-03-26 17:06:02 +0100

    fix: fix registration

--- packages/l2-contracts/scripts/register.ts
+++ packages/l2-contracts/scripts/register.ts
@@ -2,10 +2,15 @@ import { ethers } from 'ethers'
 import * as fs from 'fs'
 require('dotenv').config({ path: '../.env' })
 
-const registrarControllerAbi = JSON.parse(fs.readFileSync('./RegistrarController_abi.json', 'utf8'))
-
+const registrarControllerAbi = JSON.parse(
+  fs.readFileSync('./RegistrarController_abi.json', 'utf8'),
+)
+const resolverAbi = JSON.parse(
+  fs.readFileSync('./PublicResolver_abi.json', 'utf8'),
+)
 
 const registrarControllerAddress = '0x44411C605eb7e009cad03f3847cfbbFCF8895130'
+const resolverAddress = '0x310D7A96d8179bf4601b22299643Bf39b3fBcbb8' // PublicResolver address
 
 const provider = new ethers.providers.JsonRpcProvider(
   `https://linea-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
@@ -14,21 +19,30 @@ if (!process.env.OWNER_KEY) {
   throw new Error('OWNER_KEY environment variable is not set.')
 }
 const wallet = new ethers.Wallet(process.env.OWNER_KEY, provider)
-const registrarController = new ethers.Contract(registrarControllerAddress, registrarControllerAbi, wallet)
+const registrarController = new ethers.Contract(
+  registrarControllerAddress,
+  registrarControllerAbi,
+  wallet,
+)
+const resolver = new ethers.Contract(resolverAddress, resolverAbi, wallet)
 
 // Domain details
-const domainName = 'amine.linea'
+const domainName = 'wanted.linea'
 const duration = 365 * 24 * 60 * 60 // 1 year in seconds
-const secret = ethers.utils.formatBytes32String('SECRET') //
+const secret = ethers.utils.formatBytes32String('SECRET')
 
 async function main() {
-  // Step 1: Make a commitment
   const owner = wallet.address
-  //const resolver = ethers.constants.AddressZero;
-  const resolverAddress = '0x310D7A96d8179bf4601b22299643Bf39b3fBcbb8' // PublicResolver address
-  const data: string[] = []
   const reverseRecord = false
   const ownerControlledFuses = 0 // Fuses, 0 for no restrictions
+
+  // Encode the calls to the resolver contract
+  const data = [
+    resolver.interface.encodeFunctionData('setAddr(bytes32,address)', [
+      ethers.utils.namehash(domainName + '.eth'),
+      owner,
+    ])
+  ]
 
   const commitment = await registrarController.makeCommitment(
     domainName,
@@ -41,7 +55,7 @@ async function main() {
     ownerControlledFuses,
   )
 
-  // Step 2: Commit the commitment
+  // Commit the commitment
   const txCommit = await registrarController.commit(commitment)
   await txCommit.wait()
 
@@ -49,7 +63,7 @@ async function main() {
 
   // Wait for the minCommitmentAge + a buffer (1 minute) to ensure the commitment is ready
   setTimeout(async () => {
-    // Step 3: Register the domain
+    // Register the domain
     const txRegister = await registrarController.register(
       domainName,
       owner,
@@ -59,13 +73,15 @@ async function main() {
       data,
       reverseRecord,
       ownerControlledFuses,
-      { value: ethers.utils.parseEther('0.01') },
+      {
+        value: ethers.utils.parseEther('0.01'),
+        gasLimit: ethers.BigNumber.from('1000000'),
+      },
     )
     await txRegister.wait()
 
     console.log('Domain registered:', domainName + '.eth')
   }, (60 + 60) * 1000)
 }
-
 
 main().catch(console.error)
