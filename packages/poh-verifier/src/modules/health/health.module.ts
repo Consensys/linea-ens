205fbf674b8197cd0de778fbf89f883c95e3c7f9
wantedsystem <amine.harty@consensys.net>
2024-05-07 10:43:33 +0200

    feat: add health check

---
+++ packages/poh-verifier/src/modules/health/health.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+
+@Module({
+  imports: [TerminusModule, HttpModule],
+  controllers: [HealthController],
+  providers: [HealthService],
+})
+export class HealthModule {}
