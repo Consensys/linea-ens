e123a618405dbd6aa387fe1c58e00a426f163b9d
wantedsystem <amine.harty@consensys.net>
2024-06-11 16:10:51 +0200

    fix: fix logs

--- packages/gateway/src/utils.ts
+++ packages/gateway/src/utils.ts
@@ -1,11 +1,7 @@
-export function serializeBigInt(value: any) {
-  return typeof value === "bigint" ? value.toString() : value;
-}
-
 export function logError(error: any, ...objects: any[]) {
   const logObject = {
     error: error.message || error,
     details: objects,
   };
-  console.log(JSON.stringify(logObject, serializeBigInt, 2));
+  console.log(logObject);
 }
