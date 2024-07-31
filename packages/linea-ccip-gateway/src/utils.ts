cbe1dc47046c8af7e3b5d7939565f21278d0f6f3
Julink <julien.fontanel12@gmail.com>
2024-07-31 17:50:10 +0200

    feat: add NODE_ENV in CI

--- packages/linea-ccip-gateway/src/utils.ts
+++ packages/linea-ccip-gateway/src/utils.ts
@@ -7,7 +7,7 @@ export function logError(error: any, ...objects: any[]) {
 }
 
 export function logDebug(message: string, ...objects: any[]) {
-  if (process.env.NODE_ENV === "debug") {
+  if (process.env.NODE_ENV === "development") {
     const logObject = {
       message,
       details: objects,
