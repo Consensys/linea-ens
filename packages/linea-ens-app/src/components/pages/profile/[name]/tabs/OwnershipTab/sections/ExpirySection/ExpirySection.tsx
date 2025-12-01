96a574b44a2b8a085e5777024102ad7775e85e05
Alain Nicolas <alain.nicolas@consensys.net>
2025-12-01 18:26:14 +0100

    chore(linea-ens-app): deactivate reminder feature

--- packages/linea-ens-app/src/components/pages/profile/[name]/tabs/OwnershipTab/sections/ExpirySection/ExpirySection.tsx
+++ packages/linea-ens-app/src/components/pages/profile/[name]/tabs/OwnershipTab/sections/ExpirySection/ExpirySection.tsx
@@ -1,53 +1,12 @@
-import { CalendarEvent, google, ics, office365, outlook, yahoo } from 'calendar-link'
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
 
-import { Card, Dropdown, mq } from '@ensdomains/thorin'
+import { Card, mq } from '@ensdomains/thorin'
 
 import { cacheableComponentStyles } from '@app/components/@atoms/CacheableComponent'
-import { Button } from '@app/components/styled/Button'
 import { useNameDetails } from '@app/hooks/useNameDetails'
 
-import { EarnifiDialog } from '../../../MoreTab/Miscellaneous/EarnifiDialog'
 import { ExpiryPanel } from './components/ExpiryPanel'
-import { useExpiryActions } from './hooks/useExpiryActions'
 import { useExpiryDetails } from './hooks/useExpiryDetails'
-
-const calendarOptions = [
-  {
-    value: 'google',
-    label: 'tabs.more.misc.reminderOptions.google',
-    function: google,
-  },
-  {
-    value: 'outlook',
-    label: 'tabs.more.misc.reminderOptions.outlook',
-    function: outlook,
-  },
-  {
-    value: 'office365',
-    label: 'tabs.more.misc.reminderOptions.office365',
-    function: office365,
-  },
-  {
-    value: 'yahoo',
-    label: 'tabs.more.misc.reminderOptions.yahoo',
-    function: yahoo,
-  },
-  {
-    value: 'ics',
-    label: 'tabs.more.misc.reminderOptions.ical',
-    function: ics,
-  },
-]
-
-const makeEvent = (name: string, expiryDate: Date): CalendarEvent => ({
-  title: `Renew ${name}`,
-  start: expiryDate,
-  duration: [10, 'minute'],
-  url: window.location.href,
-})
 
 const Header = styled.div(({ theme }) => [
   css`
@@ -78,25 +37,6 @@ const PanelsContainer = styled.div(({ theme }) => [
   `),
 ])
 
-const Footer = styled.div(
-  ({ theme }) => css`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-end;
-    gap: ${theme.space['2']};
-    min-height: ${theme.space['12']};
-  `,
-)
-
-const FooterWrapper = styled.div(({ theme }) => [
-  css`
-    padding: ${theme.space['4']};
-  `,
-  mq.sm.min(css`
-    padding: ${theme.space['4']} ${theme.space['6']} ${theme.space['6']};
-  `),
-])
-
 const Container = styled.div(({ theme }) => [
   css`
     display: flex;
@@ -116,29 +56,12 @@ type Props = {
 }
 
 export const ExpirySection = ({ name, details }: Props) => {
-  const { t } = useTranslation('profile')
-
   const expiry = useExpiryDetails({ name, details })
-  const actions = useExpiryActions({
-    name,
-    expiryDetails: expiry.data,
-    ownerData: details.ownerData,
-    wrapperData: details.wrapperData,
-  })
-
-  const [showEarnifiDialog, setShowEarnifiDialog] = useState(false)
 
   if (!expiry.data || expiry.data?.length === 0) return null
 
-  const isOutOfGracePeriod = expiry.data.find((d) => d.type === 'grace-period')?.date! < new Date()
-
   return (
     <>
-      <EarnifiDialog
-        name={name}
-        open={showEarnifiDialog}
-        onDismiss={() => setShowEarnifiDialog(false)}
-      />
       <StyledCard $isCached={expiry.isCachedData}>
         <Container>
           <Header>
@@ -148,62 +71,6 @@ export const ExpirySection = ({ name, details }: Props) => {
               ))}
             </PanelsContainer>
           </Header>
-          {!isOutOfGracePeriod ? (
-            <FooterWrapper>
-              <Footer>
-                {actions?.map((action) => {
-                  if (action.type === 'set-reminder')
-                    return (
-                      <div key={action.type}>
-                        <Dropdown
-                          shortThrow
-                          keepMenuOnTop
-                          width={220}
-                          items={[
-                            {
-                              value: 'earnifi',
-                              label: t('tabs.more.misc.reminderOptions.bankless'),
-                              onClick: () => {
-                                setShowEarnifiDialog(true)
-                              },
-                            },
-                            ...calendarOptions.map((option) => ({
-                              label: t(option.label, { ns: 'profile' }),
-                              onClick: () =>
-                                window.open(
-                                  option.function(makeEvent(name, action.expiryDate)),
-                                  '_blank',
-                                ),
-                            })),
-                          ]}
-                        >
-                          <Button
-                            data-testid={`expiry-action-${action.type}`}
-                            id="remind-me-button"
-                            prefix={action.icon}
-                          >
-                            {action.label}
-                          </Button>
-                        </Dropdown>
-                      </div>
-                    )
-                  return (
-                    <div key={action.type}>
-                      <Button
-                        data-testid={`expiry-action-${action.type}`}
-                        key={action.label}
-                        prefix={action.icon}
-                        onClick={action.onClick}
-                        colorStyle={action.primary ? 'accentPrimary' : 'accentSecondary'}
-                      >
-                        {action.label}
-                      </Button>
-                    </div>
-                  )
-                })}
-              </Footer>
-            </FooterWrapper>
-          ) : null}
         </Container>
       </StyledCard>
     </>
