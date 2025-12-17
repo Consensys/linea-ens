65194022d3ae2187d5b1682fa8f12f22aaea4f48
Alain Nicolas <alainncls@users.noreply.github.com>
2025-12-17 18:22:15 +0100

    chore(global): remove deprecated packages (#363)

--- packages/poh-signer-api/src/modules/signer/signer.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SignerService } from './signer.service';
-
-@Module({
-  providers: [SignerService],
-  exports: [SignerService],
-})
-export class SignerModule {}
