0613d471bb9c5073ffba272114341921b1167a52
Alain Nicolas <alain.nicolas@consensys.net>
2026-01-09 14:36:59 +0100

    chore(global): remove migrated packages

--- packages/linea-ens-resolver/contracts/IAddrSetter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
-
-interface IAddrSetter {
-    function setAddr(
-        bytes calldata name,
-        address _addr
-    ) external view returns (bytes memory result);
-}
